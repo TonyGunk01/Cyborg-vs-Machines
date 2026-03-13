333d6ee831bc35c4861387f6eccdadbeb16dd1ad
TonyGunk01 <rishivl@hotmail.com>
2026-03-13 18:13:11 +0530

    updating scripts

--- Assets/Scripts/Traps/Laser.cs
+++ Assets/Scripts/Traps/Laser.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class LaserController : MonoBehaviour
+public class Laser : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
